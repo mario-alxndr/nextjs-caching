6445afd38f7b903b1f589d949e59c6babdd9a755
Mario Alexander <marioalexander998@gmail.com>
2026-09-07 13:29:52 +0700

    feat: cache deduplication

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,14 @@
+// import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
+
+// export const revalidate = 5;
+export const dynamic = 'force-dynamic';
+// force-dynamic => always refetch
+// force-static => would never refetch
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
